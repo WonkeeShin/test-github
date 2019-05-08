8e8a1c91b45a37ef6083f33de398d297a4e1acf0
Ondonic <vkstk0056@gmail.com>
2019-05-08 17:24:30 +0900

    add a multiply()

--- calculator.java
+++ calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
     return a + b;
   }
 
-  private static int minus(int a, int b) {
-    return a - b;
+  private static int multiply(int a, int b) {
+    return a * b;
 
 }
